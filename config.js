63ef7abdaec085dd6c7eae69877a1031ff06effb
phatdatle373 <136218473+phatdatle373@users.noreply.github.com>
2024-07-04 18:19:30 +0700

    upload

---
+++ config.js
@@ -0,0 +1,21 @@
+//====Nhập Thông Tin Của Bạn Vào Đây==============
+
+//Link Ảnh Background
+ImgLink_Background = "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/mot-em-be-tho-dang-iu-ma-sao-hoi-buon-hiu-do-1-1.jpg"
+
+//Link Ảnh Avatar
+ImgLink_Avatar = "https://i.postimg.cc/ZKCcWw9R/S-ng.png"
+
+//Nickname
+Nickname_Profile = "Đỗ Thị Thảo Sương"
+
+//Tên Thật
+RealName_Profile = ""
+
+//Các Link Của Bạn
+LinkInformation = [
+    Facebook_LinkProfile = "https://www.facebook.com/profile.php?id=100030676816283",
+    Discord_LinkProfile = "#",
+    TikTok_LinkProfile = "#",
+    Instagram_LinkProfile = "#"
+]
